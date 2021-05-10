c16848e8e383501e8460cde287ea9c440eef2141
Angelica Kursenkova <akursenkova@gmail.com>
2021-05-10 16:38:54 +0300

    failed test

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -3,6 +3,7 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.URL;
@@ -33,6 +34,8 @@ public class FirstTest {
 
     @Test
     public void firstTest(){
-        System.out.println("First test run");
+        WebElement element = driver.findElementByXPath("//*[contains(@text, 'Search Wikipedia')]");
+        element.click();
+//        System.out.println("First test run");
     }
 }
