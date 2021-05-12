d9d1754266cabca2e393a09b900b66bc5e98fc0e
Angelica Kursenkova <akursenkova@gmail.com>
2021-05-12 22:44:30 +0300

    Ex2: Создание метода
    кроме задания тут обновились библиотеки, так как мой ПК не мог работать с предыдущей версией аппиума

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -3,8 +3,11 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -34,8 +37,28 @@ public class FirstTest {
 
     @Test
     public void firstTest(){
-        WebElement element = driver.findElementByXPath("//*[contains(@text, 'Search Wikipedia')]");
-        element.click();
-//        System.out.println("First test run");
+        WebElement element_to_init_search = driver.findElementByXPath("//*[contains(@text, 'Search Wikipedia')]");
+        element_to_init_search.click();
+
+        WebElement element_to_enter_search_line = assertElementHasText(
+                "//*[contains(@text, 'Search…')]",
+                "Cannot find search input with text 'Search…'"
+        );
     }
+
+    private WebElement assertElementHasText (String xpath, String error_message, long timeoutInSeconds){
+
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        By by = By.xpath(xpath);
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
+    }
+
+    private WebElement assertElementHasText (String xpath, String error_message){
+        return assertElementHasText (xpath, error_message, 5);
+    }
+
+
 }
