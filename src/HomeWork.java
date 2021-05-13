7fa1a1789e76dcfe1d5c69c84fddd312e7e3fc1d
Angelica Kursenkova <akursenkova@gmail.com>
2021-05-13 12:15:43 +0300

    Ex2: Создание метода (assertHasText)

---
+++ src/HomeWork.java
@@ -0,0 +1,85 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.URL;
+
+public class HomeWork {
+
+    private AppiumDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "AndroidTestDevice");
+        capabilities.setCapability("platformVersion", "8.1");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("appPackage", "org.wikipedia");
+        capabilities.setCapability("appActivity", ".main.MainActivity");
+        capabilities.setCapability("app", "/home/user/work/JavaAppuimAutomation1/apks/org.wikipedia.apk");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
+
+    @Test
+    public void searchInputContainsText(){
+
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "Cannot find search input with text 'Search Wikipedia'",
+                5
+        );
+
+        assertElementHasText(
+                By.xpath("//*[contains(@text, 'Search…')]"),
+                "Search…",
+                "Cannot find search input with text 'Search…'",
+                5
+        );
+    }
+
+    private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds){
+
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
+    }
+
+    private WebElement waitForElementPresent(By by, String error_message){
+        return waitForElementPresent(by, error_message, 5);
+    }
+
+    private WebElement waitForElementAndClick(By by, String error_message, long timeoutInSeconds){
+        WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
+        element.click();
+        return element;
+    }
+
+    private void assertElementHasText(By by, String expected_text, String error_message, long timeoutInSeconds){
+        WebElement text_element = waitForElementPresent(by, error_message, timeoutInSeconds);
+        String actual_text = text_element.getAttribute("text");
+
+        Assert.assertEquals(
+                error_message,
+                expected_text,
+                actual_text
+        );
+    }
+}
