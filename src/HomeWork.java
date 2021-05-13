f9acdb453bdbadea71520cd2e13b25a0cdd4ef7c
Angelica Kursenkova <akursenkova@gmail.com>
2021-05-13 22:28:21 +0300

    Ex4*: Тест: проверка слов в поиске

--- src/HomeWork.java
+++ src/HomeWork.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
+import java.util.List;
 
 public class HomeWork {
 
@@ -53,37 +54,73 @@ public class HomeWork {
 //        );
 //    }
 
+//    @Test
+//    public void clearSearchInput(){
+//        waitForElementAndClick(
+//                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+//                "Cannot find search input with text 'Search Wikipedia'",
+//                5
+//        );
+//
+//        waitForElementAndSendKeys(
+//                By.xpath("//*[contains(@text, 'Search…')]"),
+//                "Java",
+//                "Cannot find search input",
+//                5
+//        );
+//
+//        waitForElementPresent(
+//                By.id("org.wikipedia:id/page_list_item_container"),
+//                "Cannot find search results",
+//                15
+//        );
+//
+//        waitForElementAndClick(
+//                By.id("org.wikipedia:id/search_close_btn"),
+//                "Cannot find X to cancel search",
+//                5
+//        );
+//
+//        waitForElementNotPresent(
+//                By.id("org.wikipedia:id/page_list_item_container"),
+//                "Search results are still present on the page",
+//                5
+//        );
+//    }
+
     @Test
-    public void clearSearchInput(){
+    public void testSearchResultsContainText(){
+
+        //Find search input
         waitForElementAndClick(
                 By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
                 "Cannot find search input with text 'Search Wikipedia'",
                 5
         );
 
-        waitForElementAndSendKeys(
+        //Input text for search
+        WebElement search_input_element = waitForElementAndSendKeys(
                 By.xpath("//*[contains(@text, 'Search…')]"),
                 "Java",
                 "Cannot find search input",
                 5
         );
 
+        //Make searched text a variable for convenience
+        String searched_text = search_input_element.getAttribute("text");
+
+        //Check if there are search results for our text
         waitForElementPresent(
-                By.id("org.wikipedia:id/page_list_item_container"),
-                "Cannot find search results",
+                By.xpath("//*[contains(@resource-id, 'org.wikipedia:id/page_list_item_container')]//*[contains(@text, '" + searched_text + "')]"),
+                "Cannot find search results for " + searched_text,
                 15
         );
 
-        waitForElementAndClick(
-                By.id("org.wikipedia:id/search_close_btn"),
-                "Cannot find X to cancel search",
-                5
-        );
-
-        waitForElementNotPresent(
+        //Compare number of all search results with number of results containing our text
+        assertAllSearchResultsContainSomeText(
                 By.id("org.wikipedia:id/page_list_item_container"),
-                "Search results are still present on the page",
-                5
+                By.xpath("//*[contains(@resource-id, 'org.wikipedia:id/page_list_item_container')]//*[contains(@text, '" + searched_text + "')]"),
+                "Number of results with text '" + searched_text + "' doesn't equals number of all search results"
         );
     }
 
@@ -128,6 +165,23 @@ public class HomeWork {
         wait.withMessage(error_message + "\n");
         return wait.until(
                 ExpectedConditions.invisibilityOfElementLocated(by)
+        );
+    }
+
+
+    //Method compares number of all search results with number of results containing some text
+    private void assertAllSearchResultsContainSomeText(By by1, By by2, String error_message){
+
+        List<WebElement> listOfAllResults = driver.findElements(by1);
+        int number_of_all_results = listOfAllResults.size();
+
+        List<WebElement> listOfSearchResults = driver.findElements(by2);
+        int number_of_results_with_text = listOfSearchResults.size();
+
+        Assert.assertEquals(
+                error_message,
+                number_of_all_results,
+                number_of_results_with_text
         );
     }
 
