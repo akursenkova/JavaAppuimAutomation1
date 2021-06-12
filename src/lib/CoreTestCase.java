fbce34c288dfb90359aa169ec0e1fb00513c61b2
Angelica <akursenkova@oggettoweb.com>
2021-06-12 14:45:28 +0300

    Lesson 7. Создание метода Platform и подключение skip

--- src/lib/CoreTestCase.java
+++ src/lib/CoreTestCase.java
@@ -1,30 +1,21 @@
 package lib;
 
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.android.AndroidDriver;
 import junit.framework.TestCase;
 import org.openqa.selenium.ScreenOrientation;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
-import java.net.URL;
 import java.time.Duration;
 
 public class CoreTestCase extends TestCase {
 
-    private static final String PLATFORM_IOS = "ios";
-    private static final String PLATFORM_ANDROID = "android";
-
     protected AppiumDriver driver;
-    private static String AppuimUrl = "http://127.0.0.1:4723/wd/hub";
+    protected Platform Platform;
 
     @Override
     protected void setUp() throws Exception {
 
         super.setUp();
-
-        DesiredCapabilities capabilities = this.getCapabilitiesByPlatformEnv();
-
-        driver = new AndroidDriver(new URL(AppuimUrl), capabilities);
+        this.Platform = new Platform();
+        driver = this.Platform.getDriver();
         this.rotateScreenPortrait();
     }
 
@@ -47,29 +38,5 @@ public class CoreTestCase extends TestCase {
         driver.runAppInBackground(Duration.ofMillis(millis));
     }
 
-    private DesiredCapabilities getCapabilitiesByPlatformEnv() throws Exception{
 
-        String platform = System.getenv("PLATFORM");
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-
-        if(platform.equals(PLATFORM_ANDROID)){
-            capabilities.setCapability("platformName", "Android");
-            capabilities.setCapability("deviceName", "AndroidTestDevice");
-            capabilities.setCapability("platformVersion", "8.1");
-            capabilities.setCapability("automationName", "Appium");
-            capabilities.setCapability("appPackage", "org.wikipedia");
-            capabilities.setCapability("appActivity", ".main.MainActivity");
-            capabilities.setCapability("app", "/Users/akursenkova/Documents/Work/JavaAppuimAutomation1/apks/org.wikipedia.apk");
-        } else if(platform.equals(PLATFORM_IOS)){
-            capabilities.setCapability("platformName", "iOS");
-            capabilities.setCapability("deviceName", "iPhone 8");
-            capabilities.setCapability("platformVersion", "13.4");
-            capabilities.setCapability("automationName", "Appium");
-            capabilities.setCapability("app", "/Users/akursenkova/Documents/Work/JavaAppuimAutomation1/apks/Wikipedia.app");
-        } else {
-            throw new Exception("Cannot get run platform from env variable. Platform value " + platform);
-        }
-
-        return capabilities;
-    }
 }
